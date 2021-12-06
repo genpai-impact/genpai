66a48dad1fa4760ebba68a06f6896c6ff808d95f
anan <740248416@qq.com>
2021-12-06 22:29:19 +0800

    测试提交代码

---
+++ Game equipment system/Class6.cs
@@ -0,0 +1,9 @@
+﻿public void UnloadEquipments(Equips Equipment)
+{
+    Attack -= int.Parse(Equipment.Attack.Remove(0, 1));//减去加成整数
+    Defence -= int.Parse(Equipment.Defence.Remove(0, 1));
+    Speed -= int.Parse(Equipment.Speed.Remove(0, 1));
+    MyEquips.Remove(Equipment.Adapt);//从自身装备列表中去掉该装备
+}
+
+
