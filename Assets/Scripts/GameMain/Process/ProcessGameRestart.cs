fc2d0d9b52b9b0d92cac9ee1976de1a8db0b2a75
Ranger013 <276165523@qq.com>
2022-02-23 23:28:24 +0800

    添加游戏重新开始流程脚本

---
+++ Assets/Scripts/GameMain/Process/ProcessGameRestart.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using Messager;
+using UnityEngine;
+
+namespace Genpai
+{
+    public class ProcessGameRestart : IProcess
+    {
+        private static ProcessGameRestart gameRestart = new ProcessGameRestart();
+        private ProcessGameRestart()
+        {
+        }
+        public static ProcessGameRestart GetInstance()
+        {
+            return gameRestart;
+        }
+
+        public void Dispatch(MessageArea areaCode, string eventCode, object message)
+        {
+            MessageManager.Instance.Dispatch(areaCode, eventCode, message);
+        }
+
+        public string GetName()
+        {
+            return "OnGameRestart";
+        }
+
+        public void Run()
+        {
+            
+        }
+        
+    }
+}
+
