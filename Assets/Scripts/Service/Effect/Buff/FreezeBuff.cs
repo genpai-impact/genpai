3abd1c38e46038cb92e06fac3876dcfe30f4027b
duraaaa <59920821+duraaaa@users.noreply.github.com>
2022-05-03 22:00:59 +0800

    boss冻结无法消除的问题，和感电同理，但不知道实际需求是什么样的（冻结/感电反应在boss上的体现

--- Assets/Scripts/Service/Effect/Buff/FreezeBuff.cs
+++ Assets/Scripts/Service/Effect/Buff/FreezeBuff.cs
@@ -33,8 +33,7 @@ namespace Genpai
         public override void CheckRemoval(BattleSite site)
         {
             LifeCycles--;
-            Debug.Log(LifeCycles);
-            if (target.ownerSite == site && LifeCycles <= 0)
+            if ((target.ownerSite == site || target.ownerSite == BattleSite.Boss) && LifeCycles <= 0)
             {
                 target.SelfElement = new Element(ElementEnum.Cryo);
                 DeleteBuff();
