99586bd15e3285fad865bb7cccadf72c28db6cfe
chenritiana <939512125@qq.com>
2022-05-07 21:51:07 +0800

    Card还原

--- Assets/Scripts/BattleSystem/DataScripts/Card/Card.cs
+++ Assets/Scripts/BattleSystem/DataScripts/Card/Card.cs
@@ -12,7 +12,6 @@ namespace Genpai
         public CardType cardType;
         public string cardName;
         public string[] cardInfo;
-        public int cardCnt;
 
         public Card()
         {
