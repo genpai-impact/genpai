3a59edb38e97ec76233f09b6e12f65583ca43800
REshyx <52911042+REshyx@users.noreply.github.com>
2022-07-31 22:28:16 +0800

    目录调整

---
+++ Assets/Scripts/GameGlobal/GameMain/ClickSender.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickSender : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    ClickSender() { 
+        
+    }
+
+    private void OnMouseDown()
+    {
+        Debug.Log(".............................................................................");
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
